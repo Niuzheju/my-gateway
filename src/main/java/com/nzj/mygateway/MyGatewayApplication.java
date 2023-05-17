5737d3423d245711624d7f36654df805f2bcb26b
niuzheju <niuzheju@gmail.com>
2023-05-17 16:51:19 +0800

    SpringCloud Gateway网关服务

---
+++ src/main/java/com/nzj/mygateway/MyGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.nzj.mygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class MyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGatewayApplication.class, args);
+    }
+
+}
